258ba105fd7ca94a6d8f87f70346081151210310
eseniav <esenia-v@yandex.ru>
2025-08-08 14:15:24 +0300

    add project files

---
+++ app/src/main/java/com/example/androidcrossstitchcounter/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidcrossstitchcounter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.auth_activity)
+
+        val loginBtn = findViewById<Button>(R.id.enterBtn)
+        val loginBox = findViewById<EditText>(R.id.loginTxtBox)
+
+        loginBtn.setOnClickListener {
+            val userName = loginBox.text.toString()
+            Toast.makeText(this, "Добро пожаловать, $userName!", Toast.LENGTH_SHORT).show()
+        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
