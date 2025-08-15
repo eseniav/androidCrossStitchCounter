bd9e3fce4696d5e6b2576093615884ca3a27c2e6
eseniav <esenia-v@yandex.ru>
2025-08-15 11:01:43 +0300

    add reg activity

---
+++ app/src/main/java/com/example/androidcrossstitchcounter/RegActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidcrossstitchcounter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reg_activity)
+    }
+}
