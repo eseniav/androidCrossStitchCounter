dd76cad6eab43df18913424bbdb7542c85f34274
eseniav <esenia-v@yandex.ru>
2025-08-19 11:55:02 +0300

    add login check

--- app/src/main/java/com/example/androidcrossstitchcounter/RegActivity.kt
+++ app/src/main/java/com/example/androidcrossstitchcounter/RegActivity.kt
@@ -1,6 +1,9 @@
 package com.example.androidcrossstitchcounter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,17 @@ class RegActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reg_activity)
+        val registrBtn = findViewById<Button>(R.id.regBtnRegAct)
+        val login = findViewById<EditText>(R.id.etxtLog)
+        registrBtn.setOnClickListener {
+            if(login.text.isNotEmpty()) {
+                Toast.makeText(this, "Регистрация успешно выполнена!",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Проверьте правильность заполнения формы!",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
