fd863100590b73621fd16f2cdb9699ff9a1b7c9c
eseniav <esenia-v@yandex.ru>
2025-08-23 21:24:17 +0300

    add message for changing img

--- app/src/main/java/com/example/androidcrossstitchcounter/ProfileActivity.kt
+++ app/src/main/java/com/example/androidcrossstitchcounter/ProfileActivity.kt
@@ -1,11 +1,19 @@
 package com.example.androidcrossstitchcounter
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_activity)
+
+        val avatar = findViewById<ImageView>(R.id.imgAvatar)
+
+        avatar.setOnClickListener {
+            Toast.makeText(this, "Изменение картинки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
