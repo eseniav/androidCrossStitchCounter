1f08233bb4543577a9a5ff46c4ee1bdb197eebb2
eseniav <esenia-v@yandex.ru>
2025-08-24 17:51:40 +0300

    create class & add transition to page

---
+++ app/src/main/java/com/example/androidcrossstitchcounter/ProjDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidcrossstitchcounter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProjDiaryActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.proj_diary_activity)
+    }
+}
