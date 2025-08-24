001814972ee420db4ecc6905e37cace458efa6d6
eseniav <esenia-v@yandex.ru>
2025-08-25 01:18:53 +0300

    add statistics & progress

--- app/src/main/java/com/example/androidcrossstitchcounter/ProjDiaryActivity.kt
+++ app/src/main/java/com/example/androidcrossstitchcounter/ProjDiaryActivity.kt
@@ -2,7 +2,9 @@ package com.example.androidcrossstitchcounter
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -34,6 +36,36 @@ class ProjDiaryActivity: AppCompatActivity() {
                     .setDuration(300)
                     .start()
             }
+        }
+
+        val addRow = findViewById<TableRow>(R.id.addRow)
+        val imageAdd = findViewById<ImageView>(R.id.imageAdd)
+        val imageCheck = findViewById<ImageView>(R.id.imageCheck)
+        val imageCancel = findViewById<ImageView>(R.id.imageCancel)
+
+        fun changeVisibility(isEdit: Boolean) {
+            if(isEdit) {
+                addRow.visibility = View.VISIBLE
+                imageCheck.visibility = View.VISIBLE
+                imageCancel.visibility = View.VISIBLE
+                imageAdd.visibility = View.GONE
+            } else {
+                addRow.visibility = View.GONE
+                imageCheck.visibility = View.GONE
+                imageCancel.visibility = View.GONE
+                imageAdd.visibility = View.VISIBLE
+            }
+        }
+        changeVisibility(false)
+
+        imageAdd.setOnClickListener {
+            changeVisibility(true)
+        }
+        imageCheck.setOnClickListener {
+            changeVisibility(false)
+        }
+        imageCancel.setOnClickListener {
+            changeVisibility(false)
         }
     }
 }
