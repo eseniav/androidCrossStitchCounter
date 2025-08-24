5b188bf21e1da05f74721a07162435c272c42d7f
eseniav <esenia-v@yandex.ru>
2025-08-25 01:25:26 +0300

    add page & class

---
+++ app/src/main/java/com/example/androidcrossstitchcounter/AddProjActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidcrossstitchcounter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddProjActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_proj_activity)
+    }
+}
