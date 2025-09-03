29517a9dcba75b093cc64ba5c19787ed8b215db7
eseniav <esenia-v@yandex.ru>
2025-09-03 14:08:30 +0300

    add db class

---
+++ app/src/main/java/com/example/androidcrossstitchcounter/AppDataBase.kt
@@ -0,0 +1,10 @@
+package com.example.androidcrossstitchcounter
+
+import UserDao
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDataBase: RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
