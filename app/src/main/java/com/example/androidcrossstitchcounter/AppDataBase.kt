2407e500494a83ddc7a49400abe7dc4b750d823c
eseniav <esenia-v@yandex.ru>
2025-10-02 14:39:23 +0300

    delete unused db file

--- app/src/main/java/com/example/androidcrossstitchcounter/AppDataBase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidcrossstitchcounter
-
-import User
-import androidx.room.Dao
-import androidx.room.Database
-import androidx.room.Insert
-import androidx.room.RoomDatabase
-
-
