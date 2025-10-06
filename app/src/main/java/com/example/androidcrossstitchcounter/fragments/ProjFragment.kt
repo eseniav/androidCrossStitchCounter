ea7fdbd8ac1e2761ef6be9abfae511863cbad1d5
eseniav <esenia-v@yandex.ru>
2025-10-06 15:03:50 +0300

    add proj fragment logic

--- app/src/main/java/com/example/androidcrossstitchcounter/fragments/ProjFragment.kt
+++ app/src/main/java/com/example/androidcrossstitchcounter/fragments/ProjFragment.kt
@@ -1,11 +1,14 @@
 package com.example.androidcrossstitchcounter.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.androidcrossstitchcounter.R
+import com.example.androidcrossstitchcounter.activities.ProfileActivity
+import com.example.androidcrossstitchcounter.databinding.ProjFragmentBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,9 @@ class ProjFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private val binding by lazy {
+        ProjFragmentBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +41,19 @@ class ProjFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.proj_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logProj.setOnClickListener {
+            val fragment = ProfileFragment()
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.frame, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 
     companion object {
