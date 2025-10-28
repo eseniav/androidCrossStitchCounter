1b187422dc8ac1900d2cd5098cc6159c7b855a14
eseniav <esenia-v@yandex.ru>
2025-10-28 19:05:44 +0300

    add proj diary class

---
+++ app/src/main/java/com/example/androidcrossstitchcounter/models/ProjDiary.kt
@@ -0,0 +1,43 @@
+package com.example.androidcrossstitchcounter.models
+
+import androidx.room.Dao
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Update
+import com.example.androidcrossstitchcounter.services.CalendarUtils
+
+@Entity(
+    tableName = "projDiaries",
+    foreignKeys = [
+        ForeignKey(
+            entity = Project::class,
+            parentColumns = ["id"],
+            childColumns = ["projId"]
+        )
+    ]
+)
+data class ProjDiary(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val date: String = CalendarUtils.getCurrentDateStringCompat(),
+    var crossQuantity: Int = 0,
+    val projId: Int
+)
+
+@Dao
+interface ProjDiaryDao {
+    @Insert
+    suspend fun insertProjDiary(projDiary: ProjDiary)
+    @Query("SELECT * FROM projDiaries WHERE id = :id LIMIT 1")
+    suspend fun getEntryById(id: Int): ProjDiary?
+    @Query("SELECT * FROM projDiaries WHERE projId = :projId")
+    suspend fun getProjEntriesById(projId: Int): List<ProjDiary>
+    @Query("UPDATE projDiaries " +
+            "SET crossQuantity = crossQuantity + :amount " +
+            "WHERE id = :diaryId")
+    suspend fun increaseCrossQuantity(diaryId: Int, amount: Int)
+    @Update
+    suspend fun updateProjDiary(projDiary: ProjDiary)
+}
