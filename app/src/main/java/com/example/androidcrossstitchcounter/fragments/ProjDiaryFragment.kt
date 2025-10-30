c007c264cdf654a73fb8bca7431b89a079f72f03
eseniav <esenia-v@yandex.ru>
2025-10-30 03:46:28 +0300

    extract animation & add diary dao to diary frag

--- app/src/main/java/com/example/androidcrossstitchcounter/fragments/ProjDiaryFragment.kt
+++ app/src/main/java/com/example/androidcrossstitchcounter/fragments/ProjDiaryFragment.kt
@@ -10,14 +10,23 @@ import android.widget.TableRow
 import com.example.androidcrossstitchcounter.App
 import com.example.androidcrossstitchcounter.R
 import com.example.androidcrossstitchcounter.databinding.ProjDiaryFragmentBinding
+import com.example.androidcrossstitchcounter.models.DataBaseProvider
+import com.example.androidcrossstitchcounter.models.ProjDiary
+import com.example.androidcrossstitchcounter.models.ProjDiaryDao
+import com.example.androidcrossstitchcounter.models.User
+import com.example.androidcrossstitchcounter.models.UserDao
 import com.example.androidcrossstitchcounter.services.Animation
 import com.example.androidcrossstitchcounter.services.CalendarUtils
 import com.example.androidcrossstitchcounter.services.Validation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private var projId: Int? = null
 
 /**
  * A simple [Fragment] subclass.
@@ -34,12 +43,15 @@ class ProjDiaryFragment : Fragment() {
     private val app: App by lazy {
         requireActivity().application as App
     }
+    private lateinit var diaryDao: ProjDiaryDao
+    private lateinit var diary: ProjDiary
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+            projId = it.getInt("projId")
         }
     }
 
@@ -75,17 +87,21 @@ class ProjDiaryFragment : Fragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //Анимация
+    fun Animation() {
         binding.aboutProj.setOnClickListener {
             Animation.Companion.hiding(binding.innerLayout)
         }
         binding.statistics.setOnClickListener {
             Animation.Companion.hiding(binding.statisticsInnerL)
         }
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val db = DataBaseProvider.getDB(requireContext())
+        diaryDao = db.diaryDao()
 
+        Animation()
         changeVisibility(false)
 
         binding.imageAdd.setOnClickListener {
