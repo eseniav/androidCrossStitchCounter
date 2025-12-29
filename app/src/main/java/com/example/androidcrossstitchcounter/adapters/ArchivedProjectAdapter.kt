7578306c27f744169ec1607a5ce54f81ff39a17e
eseniav <esenia-v@yandex.ru>
2025-12-29 18:39:51 +0300

    add delete dialog

--- app/src/main/java/com/example/androidcrossstitchcounter/adapters/ArchivedProjectAdapter.kt
+++ app/src/main/java/com/example/androidcrossstitchcounter/adapters/ArchivedProjectAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.androidcrossstitchcounter.adapters
 
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidcrossstitchcounter.R
 import com.example.androidcrossstitchcounter.models.ProjDao
 import com.example.androidcrossstitchcounter.models.Project
 import com.google.android.material.snackbar.Snackbar
@@ -23,7 +27,30 @@ class ArchivedProjectAdapter(private var projects: List<Project>,
     fun showDeleteConfirm() {
         Snackbar.make(view, "Проект удалён!", Snackbar.LENGTH_LONG).show()
     }
-    override fun removeItem(position: Int) {
+    override fun undoDel() {
+        onChange()
+    }
+    private  fun showDialog(position: Int, context: Context) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Удаление проекта")
+
+        // Создаём View для диалога (можно использовать layout)
+        val dialogView = LayoutInflater.from(context)
+            .inflate(R.layout.dialog_confirm_delete, null)
+
+        builder.setView(dialogView)
+
+        builder.setPositiveButton("Удалить") { _, _ ->
+            deleteProject(position)
+        }
+
+        builder.setNegativeButton("Отмена"){ _, _ ->
+            undoDel()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+    fun deleteProject(position: Int) {
         synchronized(this) {
             var notes = projects.toMutableList()
             if (position < 0 || position >= projects.size) {
@@ -42,5 +69,9 @@ class ArchivedProjectAdapter(private var projects: List<Project>,
                 }
             }
         }
+    }
+    override fun removeItem(position: Int) {
+        showDialog(position, view.context)
+
     }
 }
